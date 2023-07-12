492457d81c32517ca9e82d6755e75c5f3fc486a4
i-m-abbhay <20je0018@me.iitism.ac.in>
2023-07-13 00:32:33 +0530

    set-up the breeze and created db

--- README.md
+++ README.md
@@ -3,3 +3,29 @@
 > Started a new journey of learning Laravel during a project requirement for the internship.
 > <br>
 > Up until now I found it very interesting and easy to go with and it's kind of at your fingertips to create such amazing projects and web Apps.
+
+> ### phpunit.xml
+>
+> It's a framework for php testing.
+
+> laravel -> project
+> laravel framework -> framework
+
+## Require & Require-dev
+
+_These packages are required during the run and development time._
+
+> Just like npm install there is composer install.
+
+## .env file
+
+It's a centrallised secret file for all the secrets of our app.
+
+## breeze
+
+It's a package for authentication in laravel. You can use this after by checking out this [Link](https://laravel.com/docs/10.x/starter-kits#laravel-breeze).
+
+> ### How to connect our application to Database?
+>
+> it picks the name of db in .env file.
+> php artisan migrate -> this command will create the database for you if there is a server running mysql.
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -17,12 +17,13 @@
     </head>
     <body class="antialiased">
         <div class="relative sm:flex sm:justify-center sm:items-center min-h-screen bg-dots-darker bg-center bg-gray-100 dark:bg-dots-lighter dark:bg-gray-900 selection:bg-red-500 selection:text-white">
-            @if (Route::has('login'))
+            @if (Route::has('login')) 
+            {{-- //Shows this only we have a route named login. checkout auth.php inside routes. --}}
                 <div class="sm:fixed sm:top-0 sm:right-0 p-6 text-right z-10">
                     @auth
-                        <a href="{{ url('/home') }}" class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Home</a>
+                        <a href="{{ url('/dashboard') }}" class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Dashboard</a>
                     @else
-                        <a href="{{ route('login') }}" class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Log in</a>
+                        <a href="{{ route('login') }}" class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Sign in</a>
 
                         @if (Route::has('register'))
                             <a href="{{ route('register') }}" class="ml-4 font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Register</a>
